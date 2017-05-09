520a3d1a84b33bf7f391d3aa301f2238b84546f8
Christine Heller <heller@otris.de>
2017-05-09 09:59:34 +0200

    create additional file for login functions

--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,7 @@ import * as reduce from 'reduce-for-promises';
 import * as tsc from 'typescript-compiler';
 import * as nodeDoc from 'node-documents-scripting';
 import * as winattr from 'winattr';
+import * as login from './login';
 
 
 const open = require('open');
@@ -30,41 +31,6 @@ const NO_CONFLICT = 'No conflict';
 
 
 
-// todo debugger
-const initialConfigurations = [
-    {
-        name: 'Launch Script on Server',
-        request: 'launch',
-        type: 'janus',
-        script: '',
-        username: '',
-        password: '',
-        principal: '',
-        host: 'localhost',
-        applicationPort: 11000,
-        debuggerPort: 8089,
-        stopOnEntry: false,
-        log: {
-            fileName: '${workspaceRoot}/vscode-janus-debug-launch.log',
-            logLevel: {
-                default: 'Debug',
-            },
-        },
-    },
-    {
-        name: 'Attach to Server',
-        request: 'attach',
-        type: 'janus',
-        host: 'localhost',
-        debuggerPort: 8089,
-        log: {
-            fileName: '${workspaceRoot}/vscode-janus-debug-attach.log',
-            logLevel: {
-                default: 'Debug',
-            },
-        },
-    },
-];
 
 
 
@@ -83,7 +49,7 @@ export function activate(context: vscode.ExtensionContext) {
         launchjson = path.join(vscode.workspace.rootPath, '.vscode', 'launch.json');
     }
     let loginData: nodeDoc.LoginData = new nodeDoc.LoginData(launchjson);
-    loginData.getLoginData = createLoginData;
+    loginData.getLoginData = login.createLoginData;
     // object loginData should be deleted on deactivation
     context.subscriptions.push(loginData);
 
@@ -449,7 +415,7 @@ function commandDownloadScriptParameters(loginData: nodeDoc.LoginData, param: an
  */
 function commandSaveLoginData(loginData: nodeDoc.LoginData, param: any) {
     if(loginData) {
-        createLoginData(loginData).then(() => {
+        login.createLoginData(loginData).then(() => {
             vscode.window.setStatusBarMessage('Saved login data');
         }).catch((reason) => {
             vscode.window.showWarningMessage(reason);
@@ -1213,163 +1179,6 @@ async function ensureScript(param?: string | vscode.TextDocument): Promise<nodeD
 
 
 
-/* --------------------------------------------------
- *       Additional function to get login data
- * -------------------------------------------------- */
-
-
-async function createLoginData(_loginData: nodeDoc.LoginData): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-        askForLoginData(_loginData).then(() => {
-            createLaunchJson(_loginData).then(() => {
-                resolve();
-            }).catch((reason) => {
-                // couldn't save login data,
-                // doesn't matter, just leave a warning and continue anyway
-                vscode.window.showWarningMessage('did not save login data: ' + reason);
-                resolve();
-            });
-        }).catch((reason) => {
-            reject(reason);
-        });
-    });
-}
-
-async function askForLoginData(_loginData:nodeDoc.LoginData): Promise<void> {
-    console.log('askForLoginData');
-    
-    const SERVER: string = 'localhost';
-    const PORT: number = 11000;
-    const PRINCIPAL: string = 'dopaag';
-    const USERNAME: string = 'admin';
-    const PASSWORD = '';
-
-    return new Promise<void>((resolve, reject) => {
-
-        // showInputBox() returns a thenable(value) object,
-        // that is, these objects always have a then(value) function,
-        // value can't be empty iff it's predefined in options
-        vscode.window.showInputBox({
-            prompt: 'Please enter the server',
-            value: SERVER,
-            ignoreFocusOut: true,
-        }).then((server) => {
-            if(server) {
-                _loginData.server = server;
-                return vscode.window.showInputBox({
-                    prompt: 'Please enter the port',
-                    value: _loginData.port? _loginData.port.toString(): PORT.toString(),
-                    ignoreFocusOut: true,
-                });
-            }
-        }).then((port) => {
-            if(port) {
-                _loginData.port = Number(port);
-                return vscode.window.showInputBox({
-                    prompt: 'Please enter the principal',
-                    value: _loginData.principal? _loginData.principal: PRINCIPAL,
-                    ignoreFocusOut: true,
-                });
-            }
-        }).then((principal) => {
-            if(principal) {
-                _loginData.principal = principal;
-                return vscode.window.showInputBox({
-                    prompt: 'Please enter the username',
-                    value: _loginData.username? _loginData.username: USERNAME,
-                    ignoreFocusOut: true,
-                });
-            }
-        }).then((username) => {
-            if(username) {
-                _loginData.username = username;
-                return vscode.window.showInputBox({
-                    prompt: 'Please enter the password',
-                    value: PASSWORD,
-                    password: true,
-                    ignoreFocusOut: true,
-                });
-            }
-        }).then((password) => {
-            if(password != undefined) {
-                _loginData.password = password;
-                resolve();
-            } else {
-                reject('input login data cancelled');
-            }
-        });
-    });
-}
-
-
-
-
-
-async function createLaunchJson(_loginData:nodeDoc.LoginData): Promise<void> {
-    console.log('createLaunchJson');
-
-    return new Promise<void>((resolve, reject) => {
-        let rootPath;
-
-        if(!vscode.workspace) {
-            reject('no workspace');
-        } else {
-            rootPath = vscode.workspace.rootPath;
-        }
-
-        if(rootPath) {
-            let filename = path.join(rootPath, '.vscode', 'launch.json');
-            fs.stat(filename, function (err, stats) {
-                if(err) {
-                    if('ENOENT' === err.code) {
-                        // launch.json doesn't exist, create the default
-                        // launch.json for janus-debugger
-
-                        initialConfigurations.forEach((config: any) => {
-                            if (config.request == 'launch') {
-                                config.host = _loginData.server;
-                                config.applicationPort = _loginData.port;
-                                config.principal = _loginData.principal;
-                                config.username = _loginData.username;
-                                config.password = _loginData.password;
-                            }
-                        });
-
-                        const configurations = JSON.stringify(initialConfigurations, null, '\t')
-                            .split('\n').map(line => '\t' + line).join('\n').trim();
-
-                        const data = [
-                            '{',
-                            '\t// Use IntelliSense to learn about possible configuration attributes.',
-                            '\t// Hover to view descriptions of existing attributes.',
-                            '\t// For more information, visit',
-                            '\t// https://github.com/otris/vscode-janus-debug/wiki/Launching-the-Debugger',
-                            '\t"version": "0.2.0",',
-                            '\t"configurations": ' + configurations,
-                            '}',
-                        ].join('\n');
-
-
-                        nodeDoc.writeFile(data, filename, true).then(() => {
-                            resolve();
-                        }).catch((reason) => {
-                            reject(reason);
-                        });
-                    } else {
-                        reject(err);
-                    }
-                } else {
-                    // launch.jsaon exists
-                    // I don't dare to change it
-                    reject('cannot overwrite existing launch.json');
-                }
-            });            
-
-        } else {
-            reject('folder must be open to save login data');
-        }
-    });
-}
 
 
 
